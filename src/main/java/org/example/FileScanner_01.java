b1aef2ce77d0d9879e8300ac0f1ab498b7b776f9
nyhryan <nyhryan99102@gmail.com>
2025-09-24 22:34:49 +0900

    Initial commit

---
+++ src/main/java/org/example/FileScanner_01.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class FileScanner_01 {
+    void main() {
+        InputStream is = FileScanner_01.class.getResourceAsStream("/input.txt");
+        if (is == null) {
+            throw new RuntimeException("input.txt not found");
+        }
+
+        Scanner sc = new Scanner(is);
+        while (sc.hasNext()) {
+            System.out.printf("READ: %s\n", sc.next());
+        }
+        sc.close();
+
+        System.out.println("--------");
+
+        is = FileScanner_01.class.getResourceAsStream("/input.txt");
+        if (is == null) {
+            throw new RuntimeException("input.txt not found");
+        }
+
+        // try-resource
+        try (Scanner sc2 = new Scanner(is)) {
+            while (sc2.hasNextLine()) {
+                System.out.printf("READ: %s\n", sc2.nextLine());
+            }
+        } // <-- sc.close() called
+    }
+}
